1c247a3fa12f72b292dc7466026eb58b85eab641
Tamara Najman <tamara.najman@gmail.com>
2024-09-30 19:35:46 +0200

    Added company migration

---
+++ CraftersCloud.Blueprint.Data.Migrations/Migrations/20240930144346_CompanyEntity.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CraftersCloud.Blueprint.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompanyEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreatedById = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedById = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedOn = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Company_User_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Company_User_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Permission",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 100, "CompaniesRead" },
+                    { 101, "CompaniesWrite" },
+                    { 102, "CompaniesDelete" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RolePermission",
+                columns: new[] { "PermissionId", "RoleId" },
+                values: new object[,]
+                {
+                    { 100, new Guid("028e686d-51de-4dd9-91e9-dfb5ddde97d0") },
+                    { 101, new Guid("028e686d-51de-4dd9-91e9-dfb5ddde97d0") },
+                    { 102, new Guid("028e686d-51de-4dd9-91e9-dfb5ddde97d0") }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_CreatedById",
+                table: "Company",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_Id",
+                table: "Company",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_UpdatedById",
+                table: "Company",
+                column: "UpdatedById");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Company");
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 100, new Guid("028e686d-51de-4dd9-91e9-dfb5ddde97d0") });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 101, new Guid("028e686d-51de-4dd9-91e9-dfb5ddde97d0") });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 102, new Guid("028e686d-51de-4dd9-91e9-dfb5ddde97d0") });
+
+            migrationBuilder.DeleteData(
+                table: "Permission",
+                keyColumn: "Id",
+                keyValue: 100);
+
+            migrationBuilder.DeleteData(
+                table: "Permission",
+                keyColumn: "Id",
+                keyValue: 101);
+
+            migrationBuilder.DeleteData(
+                table: "Permission",
+                keyColumn: "Id",
+                keyValue: 102);
+        }
+    }
+}
